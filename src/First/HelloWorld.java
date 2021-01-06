ddc8a58bb76c659137079304b4a50999cdf47f32
mahavirc98 <chordiyamahavir98@gmail.com>
2021-01-06 17:15:08 +0530

    Hello WOrld Addes

--- src/First/HelloWorld.java
+++ src/First/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 		System.out.println("Mahavir Chordiya");
 		System.out.println("calling getSum");
 		
@@ -26,6 +27,7 @@ public class HelloWorld {
 		}
 		return total;
 	}
+	
 	public boolean isPalindrome(String s) {
 		String rev = new StringBuilder(s).reverse().toString();
 		return s.equals(rev);
